527153faffe6e761267c3b1303f9f844a1c08115
Giorgi <georgebolqvadze6@gmail.com>
2025-02-01 14:22:07 +0300

    new project

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:fitstyle/presentation/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FitStyle',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
