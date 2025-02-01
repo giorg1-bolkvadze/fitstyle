ea03c221cc4a1adac82ebb6537ccc150487fe3b6
Giorgi <georgebolqvadze6@gmail.com>
2025-02-01 16:35:56 +0300

    some changes

--- lib/presentation/registration_page.dart
+++ lib/presentation/registration_page.dart
@@ -1,5 +1,3 @@
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SignupPage extends StatefulWidget {
@@ -24,7 +22,8 @@ class _SignupPageState extends State<SignupPage> {
     final height = _heightController.text;
     final weight = _weightController.text;
 
-    //print('Email: $email, Password: $password, Age: $age, Height: $height, Weight: $weight');
+    print(
+        'Email: $email, Password: $password, Age: $age, Height: $height, Weight: $weight');
   }
 
   @override
@@ -88,7 +87,8 @@ class _SignupPageState extends State<SignupPage> {
                 controller: _ageController,
                 decoration: InputDecoration(
                   labelText: 'Yaş',
-                  prefixIcon: const Icon(Icons.calendar_today, color: Colors.green),
+                  prefixIcon:
+                      const Icon(Icons.calendar_today, color: Colors.green),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(15),
                   ),
@@ -118,7 +118,8 @@ class _SignupPageState extends State<SignupPage> {
                 controller: _weightController,
                 decoration: InputDecoration(
                   labelText: 'Kilo (kg)',
-                  prefixIcon: const Icon(Icons.monitor_weight, color: Colors.green),
+                  prefixIcon:
+                      const Icon(Icons.monitor_weight, color: Colors.green),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(15),
                   ),
