f4df9ca7a0df06848613ad532ae323853c58fba6
Giorgi <georgebolqvadze6@gmail.com>
2025-02-01 16:23:45 +0300

    some changes

--- lib/presentation/login_page.dart
+++ lib/presentation/login_page.dart
@@ -1,25 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'FitStyle',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        useMaterial3: true,
-      ),
-      home: const LoginPage(),
-    );
-  }
-}
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
---
+++ lib/presentation/registration_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class RegistrationPage extends StatelessWidget {
+  const RegistrationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
